915b8ae63cb21de14cc3d2b00647aa821b360f2c
Malaka <malakasilva43@gmail.com>
2020-05-08 11:36:04 +0530

    re formatted the code

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const Telegram = require("telegraf/telegram");
 
 const env = {
   GROUP_ID: -1001440768236,
-  BOT_TOKEN: "1222653443:AAGWS9lrRn-vD4OZ5lg3eo_mBFVRwNV7Ucs"
+  BOT_TOKEN: "1222653443:AAGWS9lrRn-vD4OZ5lg3eo_mBFVRwNV7Ucs",
+  CREATOR: 1007382901
 }
 
 const tg = new Telegraf(env.BOT_TOKEN);
@@ -35,15 +36,17 @@ async function _covidstat() {
   return jsonData;
 }
 
-//bot commands
+// general bot commands
 tg.start((ctx) => ctx.reply("Hello... Let's start then."))
+
 tg.hears('hi', (ctx) => {
-  if (ctx.from.id == 1007382901) {
+  if (ctx.from.id == env.CREATOR) {
     ctx.reply('My lord Murazor');
   } else {
     ctx.reply('Hello there');
   }
 })
+
 tg.hears('Hi', (ctx) => {
   if (ctx.from.id == 1007382901) {
     ctx.reply('My lord Murazor');
@@ -51,16 +54,19 @@ tg.hears('Hi', (ctx) => {
     ctx.reply('Hello there');
   }
 })
-tg.hears('covidstat', async (ctx) => {
-  _data = await _covidstat();
-  var _msg = "*CoViD19 Updates - Sri Lanka*\n\n" + "`Deaths`: " + _data.data.local_deaths + "\n`Active Cases`: " + _data.data.local_active_cases + "\n`Recovered`: " + _data.data.local_recovered + "\n`Individuals in hospitals`: " + _data.data.local_total_number_of_individuals_in_hospitals + "\n`Total Cases`: " + _data.data.local_total_cases;
-  ctx.replyWithMarkdown(_msg);
-});
+
 tg.hears('අලුත් එකෙක් ආවා', (ctx) => ctx.reply('කෝ කෝ කවුද කවුද?'))
 
 tg.hears('Hi sexy', (ctx) => {
   ctx.replyWithMarkdown('ම්ම්ම්ම්ම්ම්ම්හ්හ්හ්හ්හ්... කොල්ලෝඕඕඕඔහ්හ්හ්හ්හ්හ්....');
 })
+
+// covid-19 related commands
+tg.hears('covidstat', async (ctx) => {
+  _data = await _covidstat();
+  var _msg = "*CoViD19 Updates - Sri Lanka*\n\n" + "`Deaths`: " + _data.data.local_deaths + "\n`Active Cases`: " + _data.data.local_active_cases + "\n`Recovered`: " + _data.data.local_recovered + "\n`Individuals in hospitals`: " + _data.data.local_total_number_of_individuals_in_hospitals + "\n`Total Cases`: " + _data.data.local_total_cases;
+  ctx.replyWithMarkdown(_msg);
+});
 
 tg.hears('Covidstat', async (ctx) => {
   _data = await _covidstat();
@@ -68,10 +74,8 @@ tg.hears('Covidstat', async (ctx) => {
   ctx.replyWithMarkdown(_msg);
 });
 
-
 tg.launch();
 
-// Finally, start our server
 app.listen(server_port, () => {
   console.log("Telegram app listening on port 3000!");
 
